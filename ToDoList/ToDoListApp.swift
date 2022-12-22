ccbc861a38c22856683f3742dd3df8ef0aad5ec7
thaxz <thais.monteiroc@ufpe.br>
2022-12-21 21:36:57 -0300

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by thaxz on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+        }
+    }
+}
---
+++ ToDoList/Views/ListView.swift
@@ -0,0 +1,22 @@
+//
+//  ListView.swift
+//  ToDoList
+//
+//  Created by thaxz on 21/12/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            ListView()
+        }
+    }
+}
